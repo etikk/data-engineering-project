5a391b294b63b19cdf556fdfb834f947a373ca9b
MVTU <144995919+MVTU@users.noreply.github.com>
2023-12-28 20:56:08 +0200

    init airflow update

--- init_airflow.sh
+++ init_airflow.sh
@@ -5,11 +5,22 @@
 # Load environment variables from .env file
 export $(cat .env | xargs)
 
+echo "AIRFLOW_USERNAME: $AIRFLOW_USERNAME"
+echo "AIRFLOW_PASSWORD: $AIRFLOW_PASSWORD"
+echo "AIRFLOW_FIRSTNAME: $AIRFLOW_FIRSTNAME"
+echo "AIRFLOW_LASTNAME: $AIRFLOW_LASTNAME"
+echo "AIRFLOW_ROLE: $AIRFLOW_ROLE"
+echo "AIRFLOW_EMAIL: $AIRFLOW_EMAIL"
+echo "POSTGRES_DB: $POSTGRES_USER"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo "POSTGRES_USER : $POSTGRES_USER"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+
 # Initialize Airflow DB
-docker-compose run airflow-webserver airflow db init
+docker-compose exec airflow-webserver airflow db init  
 
 # Create Airflow user
-docker-compose run airflow-webserver airflow users create \
+docker-compose exec airflow-webserver airflow users create \
   --username $AIRFLOW_USERNAME \
   --password $AIRFLOW_PASSWORD \
   --firstname $AIRFLOW_FIRSTNAME \
