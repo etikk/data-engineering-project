0451fe07526310fdbdb8332d339bff861e7a06ee
MVTU <144995919+MVTU@users.noreply.github.com>
2023-12-29 01:11:49 +0200

    Graph

--- dags/save_data_neo4j_dag.py
+++ dags/save_data_neo4j_dag.py
@@ -20,8 +20,12 @@ def save_data_to_neo4j():
 
     driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    with open('/app/raw_data/chunk_1.json') as f:
-        data = json.load(f)
+    try:
+        with open('/app/raw_data/new_partition_0.json') as f:
+            data = [json.loads(line) for line in f]
+    except FileNotFoundError:
+        print("The file was not found.")
+        return
 
     # data = data[:1000]
 
@@ -30,15 +34,70 @@ def save_data_to_neo4j():
             # Handle non-primitive types and hyphens
             item = serialize_non_primitive(item)
             item_with_underscores = {key.replace('-', '_'): value for key, value in item.items()}
+            if item_with_underscores.get('authors_parsed'):
+                authors_parsed_query = """
+                    MERGE (b:Title {Title: $title})
+                    WITH b, $authors_parsed AS authors_parsed
+                    UNWIND authors_parsed AS author_parsed
+                    MERGE (a:Author {Name: author_parsed})
+                    ON CREATE SET a.Name = author_parsed
+                    MERGE (a)-[:Authored]->(b)
+                """
 
-            # Check if a node with the same 'id' exists
-            id_check_query = "MATCH (a:Article) WHERE a.id = $id RETURN a"
-            id_check_result = session.run(id_check_query, id=item_with_underscores['id'])
-            if not id_check_result.single():
-                # Dynamically construct properties for the CREATE query
-                properties = ', '.join([f"{key}: ${key}" for key in item_with_underscores.keys()])
-                create_query = f"CREATE (a:Article {{ {properties} }})"
-                session.run(create_query, **item_with_underscores)
+                #MERGE (a:Author {firstName: author_parsed[1], lastName: author_parsed[0]})
+
+                session.run(authors_parsed_query, title=item_with_underscores['title'], authors_parsed=item_with_underscores['authors_parsed'])
+
+
+            if item_with_underscores.get('submitter'):
+                submitter_parsed_query = """
+                    MERGE (b:Title {Title: $title})
+                    WITH b, $submitter AS submitter_parsed
+                    UNWIND submitter_parsed AS new_submitter_parsed
+                    MERGE (c:Author {Name: new_submitter_parsed})
+                    ON CREATE SET c.Name = new_submitter_parsed
+                    MERGE (c)-[:SUBMITTED]->(b)
+                """
+                #MERGE (c:Author {firstName: submitter_parsed[1], lastName: submitter_parsed[0]})
+                session.run(submitter_parsed_query,title=item_with_underscores['title'], submitter=item_with_underscores['submitter'])
+
+            # MERGE for Category_List
+            if item_with_underscores.get('Category_List'):
+                category_list_query = """
+                    MERGE (b:Title {Title: $title})
+                    WITH b, $Category_List AS categories
+                    MERGE (d:Category_List {Category_List: categories}) 
+                    ON CREATE SET d.Category_List = categories
+                    MERGE (b)-[:IS_CATEGORIZED_AS]->(d)
+                """
+                session.run(category_list_query,title=item_with_underscores['title'], Category_List=item_with_underscores['Category_List'])
+
+            # Cypher query for Disciplines
+            if item_with_underscores.get('Disciplines'):
+                disciplines_query = """
+                    MERGE (b:Title {Title: $title})
+                    WITH b, $disciplines AS disciplines
+                    UNWIND disciplines AS discipline
+                    MERGE (f:Discipline {Discipline: discipline})
+                    ON CREATE SET f.Discipline = discipline
+                    MERGE (b)-[:IS_IN_DISCIPLINE]->(f)
+                """
+                session.run(disciplines_query, title=item_with_underscores['title'],disciplines=item_with_underscores['Disciplines'])
+
+            # Cypher query for Referenced_In_Journal with weight
+            if item_with_underscores.get('journal_ref'):
+                referenced_in_journal_query = """
+                    MERGE (b:Title {Title: $title})
+                    WITH b, $journal_ref AS journal_ref, $Publication_Type AS publication_type, $ReferencedByCount AS referenced_by_count
+                    MERGE (e:Journal_Reference {Journal_Reference: coalesce(journal_ref, 'Unknown'), Publication_Type: coalesce(publication_type, 'Unknown')})
+                    MERGE (b)-[r:Referenced_In_Journal]->(e)
+                    SET r.weight = referenced_by_count
+                """
+                session.run(referenced_in_journal_query, 
+                            journal_ref=item_with_underscores['journal_ref'],
+                            Publication_Type=item_with_underscores['PublicationType'],
+                            ReferencedByCount=item_with_underscores['ReferencedByCount'],
+                            title=item_with_underscores['title'])
 
     driver.close()
 
--- helpers/Graph_Query.cypher
+++ helpers/Graph_Query.cypher
@@ -1,26 +1,33 @@
 //Main script
 CALL apoc.load.json("file:/partition_test.json") YIELD value AS author
 MERGE (b:Title {Title: author.title})  // Title
-//SET b.Title = author.title
+
 WITH b,author
 UNWIND author.authors_parsed AS author_parsed
 MERGE (a:Author {firstName: author_parsed[1], lastName: author_parsed[0]})
 MERGE (a)-[:Authored]->(b)
+
 WITH b,author
 UNWIND author.submitter as sumbitter_parsed
 MERGE (c:Author {firstName: sumbitter_parsed[1], lastName: sumbitter_parsed[0]})  // Submitter
-//SET c.submitter = author.submitter
 MERGE (c)-[:SUBMITTED]->(b)
+
 WITH b,author
-WHERE author.categories IS NOT NULL
-MERGE (d:Categories {Categories : author.categories}) // Categories
-//SET d.Categories = author.categories
+WHERE author.Category_List IS NOT NULL
+MERGE (d:Category_List {Category_List : author.categories}) 
 MERGE (b)-[:IS_CATEGORIZED_AS]->(d)
+
+WITH b, author
+WHERE author.Disciplines IS NOT NULL
+UNWIND author.Disciplines AS discipline
+MERGE (f:Discipline {Discipline: discipline})
+MERGE (b)-[:IS_IN_DISCIPLINE]->(f)
+
 WITH b,author
 WHERE author.journal_ref IS NOT NULL
-MERGE (e:Journal_Reference {Journal_Reference : author.journal_ref}) // Journal_Ref
-//SET d.Categories = author.categories
-MERGE (b)-[:Referenced_In_Journal]->(e)
+MERGE (e:Journal_Reference {Journal_Reference : author.journal_ref, Publication_Type : author.PublicationType}) // Journal_Ref
+MERGE (b)-[r:Referenced_In_Journal]->(e)
+SET r.weight = author.ReferencedByCount;
 
 
 
